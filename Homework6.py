b0cd911c179088479e4a69e9fe6046c965bbc82a
GIUS0 <cepesv333@gmail.com>
2024-05-24 19:43:03 +0700

    Add files via upload

---
+++ Homework6.py
@@ -0,0 +1,13 @@
+my_list=['apple','banana','coconut','pineapple','lemon','mango']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:6])
+my_list[2]='orange'
+print(my_list)
+
+my_dict= {'apple':'яблоко', 'banana':'банан','coconut':'кокос'}
+print(my_dict)
+print(my_dict['apple'])
+my_dict['peach']= 'персик'
+print(my_dict)
