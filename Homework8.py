f06f2cd250aefa1eed363ed1a9520fab28ee0151
GIUS0 <cepesv333@gmail.com>
2024-05-29 00:20:00 +0700

    Update Homework8.py

--- Homework8.py
+++ Homework8.py
@@ -2,5 +2,7 @@ grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 new_students=list(students)
 new_students.sort()
-new_grades=sum(grades[0])/len(grades[0])
-
+key=new_students[0]
+value=sum(grades[0])/len(grades[0])
+new_grades={key:value}
+print(new_grades)
