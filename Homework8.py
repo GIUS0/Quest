5b26b2523cd97e6463dc72de131aec5fe63802de
GIUS0 <cepesv333@gmail.com>
2024-05-29 00:08:49 +0700

    Add files via upload

---
+++ Homework8.py
@@ -0,0 +1,6 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+new_students=list(students)
+new_students.sort()
+new_grades=sum(grades[0])/len(grades[0])
+
