24af5f2c85d29c8eb485752da3ec08b07e48ca3d
GIUS0 <cepesv333@gmail.com>
2024-07-03 16:45:04 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import true_math as tm
-import fake_math as fm
 
-result1 = tm.divide2(10, 0)
-result2 = tm.divide2(12, 6)
-result3 = fm.divide1(15, 0)
-result4 = fm.divide1(16, 2)
-print(result1)
-print(result2)
-print(result3)
-print(result4)
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+    inner_function()
+
+
+test_function()
