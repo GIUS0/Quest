102b21e0f3533252a9eebc7ebf1367a786d3ed7b
GIUS0 <cepesv333@gmail.com>
2024-07-28 11:22:53 +0700

    Add files via upload

--- Homework 5.1.py
+++ Homework 5.1.py
@@ -11,7 +11,24 @@ class House:
                 print('Такого этажа не существует')
                 break
 
+    def __str__(self):
+        return self.name 
+
+    def __len__(self):
+        return self.number_of_floors
+
+
+
+
+
+
 h1 = House('ЖК Рубин', 20)
 h2 = House('Домик в деревне', 3)
 h1.go_to(5)
 h2.go_to(10)
+
+print(len(h1))
+print(len(h2))
+
+print(h1)
+print(h2)
